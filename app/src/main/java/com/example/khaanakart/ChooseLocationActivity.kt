bb68eb441bb814ba4d74bddbef5a38593b4b4cab
Arghadeep Chakraborty <argho.chakr@gmail.com>
2024-08-15 03:47:05 -0400

    Location Screen UI Customer List Item UI

--- app/src/main/java/com/example/khaanakart/ChooseLocationActivity.kt
+++ app/src/main/java/com/example/khaanakart/ChooseLocationActivity.kt
@@ -1,20 +1,25 @@
 package com.example.khaanakart
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.khaanakart.databinding.ActivityChooseLocationBinding
 
 class ChooseLocationActivity : AppCompatActivity() {
+
+    private val binding: ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_choose_location)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        val locationList: Array<String> = arrayOf("Noida", "Greater Noida", "New Delhi", "Gurugram", "Faridabad", "Ghaziabad")
+        val adapter = ArrayAdapter(this, R.layout.dropdown_list_item, locationList)
+        val autoCompleteTextView = binding.autoCompleteLocationList
+        autoCompleteTextView.setAdapter(adapter)
+
     }
 }
